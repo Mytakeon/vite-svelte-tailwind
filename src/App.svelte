2057c9ebe5c07d100a5841d32460b99e9332d0ca
Arthur Alglave <aalglave@microsoft.com>
2022-05-01 22:45:08 +0100

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import logo from "./assets/svelte.png";
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main class="w-2/4 mx-auto text-center">
+  <img class="w-60 mx-auto" src={logo} alt="Svelte Logo" />
+  <h1 class="text-orange-500 uppercase text-6xl font-thin my-8">
+    Hello Typescript!
+  </h1>
+
+  <Counter />
+
+  <p class="py-4">
+    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+    apps.
+  </p>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
+    the officially supported framework, also powered by Vite!
+  </p>
+</main>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let count: number = 0;
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<button
+  class="text-red-500 bg-red-50 cursor-pointer w-52 rounded-3xl px-5 py-3 focus:border-2 focus:border-orange-500 active:bg-orange-100"
+  on:click={increment}
+>
+  Clicks: {count}
+</button>
